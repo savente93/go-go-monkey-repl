3f702f69e6c45657f2fd399fed90693eeb5be9d9
Sam Vente <savente93@gmail.com>
2023-08-31 16:32:44 +0200

    finish chapter 1

--- src/monkey/main.go
+++ src/monkey/main.go
@@ -2,11 +2,17 @@ package main
 
 import (
 	"fmt"
-
-	"monkey/lexer"
+	"monkey/repl"
+	"os"
+	"os/user"
 )
 
 func main() {
-	lexer := lexer.New("a + b")
-	fmt.Println(lexer)
+	user, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Hello %s! This is the Monkey programming language!\n", user.Username)
+	fmt.Printf("Feel free to type in commands\n")
+	repl.Start(os.Stdin, os.Stdout)
 }
